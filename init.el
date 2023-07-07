9905147e7ac26d5833f108909894c2a1c75194d8
Harper Mascarenhas <harperbolic@proton.me>
2023-07-07 03:56:09 -0300

    make emacs simple starting point

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
-'(femboy)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")))
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)     ;Disable visible scrollbar
+(tool-bar-mode -1)       ;Disable tooltip bar
+(tooltip-mode -1)        ;Disable tooltips
+(set-fringe-mode -1)     ;Give breathing room
+
+(menu-bar-mode -1)       ;Disable menu bar
+
+;;Set visible bell to disable annoying beeps
+(setq visible-bell t)
+
