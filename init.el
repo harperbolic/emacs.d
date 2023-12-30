a006bb6b6f8ac969dc49b6aeb4e44a14a251ba1a
Harper Mascarenhas <105064849+harperbolic@users.noreply.github.com>
2023-12-30 16:35:04 -0300

    Update init.el

--- init.el
+++ init.el
@@ -67,26 +67,6 @@
 ;(require 'exwm-config)
 ;(exwm-config-default)
  
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(auto-complete exwm ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;;autocomplete
-(ac-config-default)
-
 ;;Discord rich presence
 (require 'elcord)
 (elcord-mode)
-
-;;Unity compatibility
